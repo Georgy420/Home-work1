eb745fdd975dc6a3e9ffcdecada52de9bc3cc931
Georgy <geo.dz420@gmail.com>
2020-11-13 12:19:29 +0300

    v1

---
+++ 3_quest.py
@@ -0,0 +1,20 @@
+n = int(input("Введите число n: "))
+temp = str(n)
+g1 = temp + temp
+g2 = temp + temp + temp
+result = n + int(g1) + int(g2)
+print(f"Результат сложения: {n} + {g1} + {g2} равен:", result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
